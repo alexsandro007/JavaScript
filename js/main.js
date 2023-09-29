ccc57893049da735da9299c26eed939f7fd97516
alexsandro007 <yarmolaaleksandr@gmail.com>
2023-09-29 08:10:29 +0300

    updated the code and added an additional check

--- js/main.js
+++ js/main.js
@@ -19,74 +19,82 @@ kru2.innerHTML = finalAnswer;
 function inp() {
     
     let str = input.value;
-    let arr = [ 0, 0, 0, 0 ];
-    for(let i = 0; i < 4; i++){
-        arr[i] = Number(str[i]) ;
-    }
-    console.log(arr);
+    let str2 = input.value;
 
-    let first = 0, second = 0, third = 0, four = 0;
+    let first = -1, second = -1, third = -1;
 
     for(let i = 0; i < 4; i++){
-        if(arr[i] > first && arr[i] <= 2){
-            first = arr[i];
-            arr[i] = arr[0];
-            arr[0] = first;
-        }
+        if(str[i] > first && str[i] <= 2)
+            first = str[i];
     }
+    str = str.replace(first, '');
 
-    if(arr[0] <= 2){
+    if(str.length < 4){
         if(first <= 1){
-            for(let i = 1; i < 4; i++){
-                if(arr[i] > second && arr[i] <= 9){
-                    second = arr[i];
-                    arr[i] = arr[1];
-                    arr[1] = second;
-                }
+            for(let i = 0; i < 3; i++){
+                if(str[i] > second && str[i] <= 9)
+                    second = str[i];
             }
+            str = str.replace(second, '');
         }
         else{
-            for(let i = 1; i < 4; i++){
-                if(arr[i] > second && arr[i] <= 3){
-                    second = arr[i];
-                    arr[i] = arr[1];
-                    arr[1] = second;
-                }
+            for(let i = 0; i < 3; i++){
+                if(str[i] > second && str[i] <= 3)
+                    second = str[i];
             }
+            str = str.replace(second, '');
         }
 
-        if(arr[0] == 2 && arr[1] <= 3){
-            for(let i = 2; i < 4; i++){
-                if(arr[i] > third && arr[i] <= 5){
-                    third = arr[i];
-                    arr[i] = arr[2];
-                    arr[2] = third;
-                }
+        if(str.length < 3){
+            for(let i = 0; i < 2; i++){
+                if(str[i] > third && str[i] <= 5)
+                    third = str[i];
             }
-            if(arr[2] <= 5)
-                finalAnswer = `${arr[0]}${arr[1]}:${arr[2]}${arr[3]}`;
+            str = str.replace(third, '');
+        }
+        else
+            finalAnswer = "error";
+        
+        if(str.length == 1)
+            finalAnswer = `${first}${second}:${third}${str}`;
+        else
+            finalAnswer = "error";
+    }
+    else
+        finalAnswer = "error";
+    //For example:
+    //If we have str = "1266", the code will process like this first = 2, second = 1, and we will get an "error" in the response. But the correct answer "16:26".
+    //So we have to assign the value 1 to the variable first(first = 1), and then continue to compose the correct value.
+    if(finalAnswer == "error" && first == 2 && second == 1){
+        first = 1;
+        str2 = str2.replace(first, '');
+
+        if(str2.length < 4){
+
+            for(let i = 0; i < 3; i++){
+                if(str2[i] > second && str2[i] <= 9)
+                    second = str2[i];
+            }
+            str2 = str2.replace(second, '');
+    
+            if(str2.length < 3){
+                for(let i = 0; i < 2; i++){
+                    if(str2[i] > third && str2[i] <= 5)
+                        third = str2[i];
+                }
+                str2 = str2.replace(third, '');
+            }
             else
                 finalAnswer = "error";
-        }
-        else if(arr[0] <= 1 && arr[1] <= 9){
-            for(let i = 2; i < 4; i++){
-                if(arr[i] > third && arr[i] <= 5){
-                    third = arr[i];
-                    arr[i] = arr[2];
-                    arr[2] = third;
-                }
-            }
-            if(arr[2] <= 5)
-                finalAnswer = `${arr[0]}${arr[1]}:${arr[2]}${arr[3]}`;
+            
+            if(str2.length == 1)
+                finalAnswer = `${first}${second}:${third}${str2}`;
             else
                 finalAnswer = "error";
         }
         else
             finalAnswer = "error";
     }
-    else
-        finalAnswer = "error";
-
 
     kru2.innerHTML = finalAnswer;
     console.log(finalAnswer);
